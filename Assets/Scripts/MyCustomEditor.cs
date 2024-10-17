1f647cf2bc07d14d941d8159be046fa7ded83f05
WilLa03 <a22willa@student.his.se>
2024-10-17 23:41:28 +0200

    Can now undo

--- Assets/Scripts/MyCustomEditor.cs
+++ Assets/Scripts/MyCustomEditor.cs
@@ -28,6 +28,7 @@ public class MyCustomEditor : EditorWindow
     private List<ItemDefinition> allItems;
     private Sorting sortingtype = Sorting.Alphabet;
     [SerializeField] private Sprite defaultSprite;
+    private ItemDefinition currentSc;
 
     [MenuItem("Window/ItemManager")]
     public static void ShowMyEditor()
@@ -47,9 +48,16 @@ public class MyCustomEditor : EditorWindow
         var defaultsprites = AssetDatabase.FindAssets("defaultsprite");
 
         defaultSprite = AssetDatabase.LoadAssetAtPath<Sprite>(AssetDatabase.GUIDToAssetPath(defaultsprites[0]));
+        Undo.undoRedoPerformed += OnUndoRedoPerformed;
+    }
+
+    private void OnDisable()
+    {
+        Undo.undoRedoPerformed -= OnUndoRedoPerformed;
     }
 
     
+
 
     private List<ScriptableObject> FindScriptable()
     {
@@ -157,181 +165,204 @@ public class MyCustomEditor : EditorWindow
     
     
 
-    private void OnItemSelectionChange(IEnumerable<object> selectedItems)
-  {
-      
-      // Clear all previous content from the pane.
-      m_RightPane.Clear();
-      
-      
-      
-      var enumerator = selectedItems.GetEnumerator();
-      if (enumerator.MoveNext())
-      {
-          
-          var sc = enumerator.Current as ItemDefinition;
-          if (sc != null)
-          {
-              AssetDatabase.RenameAsset(path, newAssetName);
-              ItemsGuids = AssetDatabase.FindAssets(sc.name);
-              path = AssetDatabase.GUIDToAssetPath(ItemsGuids[0]);
-              selectedName = sc.name;
+   private void OnItemSelectionChange(IEnumerable<object> selectedItems)
+{
+    // Clear all previous content from the pane.
+    m_RightPane.Clear();
+    
+    var enumerator = selectedItems.GetEnumerator();
+    if (enumerator.MoveNext())
+    {
+        var sc = enumerator.Current as ItemDefinition;
+        if (sc != null)
+        {
+            currentSc = sc;
+            // Begin recording changes for undo functionality
+            Undo.RecordObject(sc, "Modify Item");
 
+            AssetDatabase.RenameAsset(path, newAssetName);
+            ItemsGuids = AssetDatabase.FindAssets(sc.name);
+            path = AssetDatabase.GUIDToAssetPath(ItemsGuids[0]);
+            selectedName = sc.name;
 
-              var Aname = new TextField("Name of asset");
-              Aname.RegisterValueChangedCallback(evt =>
-              {
-                  newAssetName = Aname.value;
-                  Debug.Log(Aname.maxLength);
-                  
-              });
-              Aname.value = sc.name;
-              Aname.style.alignSelf = Align.Stretch;
-              Aname.style.marginTop = 10;
-              
-              var name = new TextField("Name of item");
-              name.maxLength = 20;
-              name.RegisterValueChangedCallback(evt =>
-              {
-                  sc.FriendlyName = name.value;
-                  EditorUtility.SetDirty(sc);
-              });
-              name.value = sc.FriendlyName;
-              name.style.alignSelf = Align.Stretch;
-              name.style.marginTop = 10;
+            var Aname = new TextField("Name of asset");
+            Aname.RegisterValueChangedCallback(evt =>
+            {
+                Undo.RecordObject(sc, "Rename Asset");
+                newAssetName = Aname.value;
+            });
+            Aname.name = "Asset Name";
+            Aname.value = sc.name;
+            Aname.style.alignSelf = Align.Stretch;
+            Aname.style.marginTop = 10;
 
-              var rarity = new EnumField("Rarity of item",sc.Rarity);
-              
-              rarity.RegisterCallback<ChangeEvent<Enum>>(evt =>
-              {
-                  sc.Rarity = (Rarity)Enum.Parse(typeof(Rarity), evt.newValue.ToString());
-                  EditorUtility.SetDirty(sc);
-              });
-              
-              rarity.style.alignSelf = Align.Stretch;
+            var name = new TextField("Name of item");
+            name.maxLength = 20;
+            name.RegisterValueChangedCallback(evt =>
+            {
+                Undo.RecordObject(sc, "Change Item Name");
+                sc.FriendlyName = name.value;
+                EditorUtility.SetDirty(sc);
+            });
+            name.value = sc.FriendlyName;
+            name.style.alignSelf = Align.Stretch;
+            name.style.marginTop = 10;
 
+            var rarity = new EnumField("Rarity of item", sc.Rarity);
+            rarity.RegisterCallback<ChangeEvent<Enum>>(evt =>
+            {
+                Undo.RecordObject(sc, "Change Item Rarity");
+                sc.Rarity = (Rarity)Enum.Parse(typeof(Rarity), evt.newValue.ToString());
+                EditorUtility.SetDirty(sc);
+            });
+            rarity.style.alignSelf = Align.Stretch;
 
-              var description = new TextField("Description of item");
-              description.RegisterValueChangedCallback(evt =>
-              {
-                  sc.Description = description.value;
-                  EditorUtility.SetDirty(sc);
-              });
-              description.value = sc.Description;
-              description.multiline = true;
-              description.style.alignSelf = Align.Stretch;
-              
-              var sellprice = new IntegerField("Sell price for item");
-              sellprice.RegisterValueChangedCallback(evt =>
-              {
-                  sc.SellPrice = sellprice.value;
-                  EditorUtility.SetDirty(sc);
-              });
-              sellprice.value = sc.SellPrice;
-              sellprice.style.alignSelf = Align.Stretch;
+            var description = new TextField("Description of item");
+            description.RegisterValueChangedCallback(evt =>
+            {
+                Undo.RecordObject(sc, "Change Item Description");
+                sc.Description = description.value;
+                EditorUtility.SetDirty(sc);
+            });
+            description.value = sc.Description;
+            description.multiline = true;
+            description.style.alignSelf = Align.Stretch;
 
-              
-              var icon = new ObjectField("Icon for item");
-              icon.RegisterValueChangedCallback(evt =>
-              {
-                  sc.Icon = icon.value as Sprite;
-                  EditorUtility.SetDirty(sc);
-              });
-              icon.objectType = typeof(Sprite);
-              icon.value = sc.Icon;
-              icon.style.alignSelf = Align.Stretch;
-              
-              
-              
-              var iconPre = new UnityEngine.UIElements.Image();
-              iconPre.sprite = sc.Icon;
-              iconPre.style.height = sc.SlotDimension.y * 50;
-              iconPre.style.width = sc.SlotDimension.x * 50;
-              iconPre.style.alignSelf = Align.Center;
-              iconPre.style.marginBottom = 20;
-              iconPre.style.marginTop = 20;
-              iconPre.style.backgroundColor = new StyleColor(new Color(0,0,0,0.2f));
-              iconPre.scaleMode = ScaleMode.ScaleToFit;
-              
-              
-              var dimensions = new Vector2IntField("Dimensions for item");
-              dimensions.RegisterValueChangedCallback(evt =>
-              {
-                  if (dimensions.value.x < 1)
-                  {
-                      dimensions.value = new Vector2Int(1,dimensions.value.y);
-                  }
-                  else if (dimensions.value.y < 1)
-                  {
-                      dimensions.value = new Vector2Int(dimensions.value.x,1);
-                  }
-                  else if(dimensions.value.x > 9)
-                  {
-                      dimensions.value = new Vector2Int(9,dimensions.value.y);
-                  }
-                  else if(dimensions.value.y > 6)
-                  {
-                      dimensions.value = new Vector2Int(dimensions.value.x,6);
-                  }
-                  else
-                  {
-                      sc.SlotDimension.x = dimensions.value.x;
-                      sc.SlotDimension.y = dimensions.value.y;
-                      iconPre.style.height = sc.SlotDimension.y * 50;
-                      iconPre.style.width = sc.SlotDimension.x * 50;
-                      EditorUtility.SetDirty(sc);
-                  }
-                  
-              });
-              dimensions.value = new Vector2Int(sc.SlotDimension.x, sc.SlotDimension.y);
-              dimensions.style.alignSelf = Align.Stretch;
+            var sellprice = new IntegerField("Sell price for item");
+            sellprice.RegisterValueChangedCallback(evt =>
+            {
+                Undo.RecordObject(sc, "Change Sell Price");
+                sc.SellPrice = sellprice.value;
+                EditorUtility.SetDirty(sc);
+            });
+            sellprice.value = sc.SellPrice;
+            sellprice.style.alignSelf = Align.Stretch;
 
-              
-             
-              
-              
-              
-              
-              
-              
-              var Save = new Button();
-              Save.text = "Save";
-              Save.style.marginTop = 30;
-              Save.style.alignSelf =  Align.Center;
-              Save.style.maxWidth = 400;
-              Save.style.minWidth = 300;
-              Save.style.maxHeight = 20;
-              Save.style.minHeight = 20;
-              Save.style.flexGrow = 1;
-              
-              var Delete = new Button();
-              Delete.text = "Delete Item";
-              Delete.style.marginTop = 10;
-              Delete.style.maxWidth = 400;
-              Delete.style.minWidth = 300;
-              Delete.style.alignSelf =  Align.Center;
-              Delete.style.maxHeight = 20;
-              Delete.style.minHeight = 20;
-              Delete.style.flexGrow = 1;
-              
-              
-              m_RightPane.Add(Aname);
-              m_RightPane.Add(name);
-              m_RightPane.Add(rarity);
-              m_RightPane.Add(description);
-              m_RightPane.Add(sellprice);
-              m_RightPane.Add(dimensions);
-              m_RightPane.Add(icon);
-              //m_RightPane.Add(Save);
-              m_RightPane.Add(iconPre);
-              m_RightPane.Add(Delete);
+            
+            var iconPre = new UnityEngine.UIElements.Image();
+            iconPre.sprite = sc.Icon;
+            iconPre.style.height = sc.SlotDimension.y * 50;
+            iconPre.style.width = sc.SlotDimension.x * 50;
+            iconPre.style.alignSelf = Align.Center;
+            iconPre.style.marginBottom = 20;
+            iconPre.style.marginTop = 20;
+            iconPre.style.backgroundColor = new StyleColor(new Color(0, 0, 0, 0.2f));
+            iconPre.scaleMode = ScaleMode.ScaleToFit;
+            
+            var icon = new ObjectField("Icon for item");
+            icon.RegisterValueChangedCallback(evt =>
+            {
+                Undo.RecordObject(sc, "Change Icon");
+                sc.Icon = icon.value as Sprite;
+                iconPre.sprite = icon.value as Sprite;
+                EditorUtility.SetDirty(sc);
+            });
+            icon.objectType = typeof(Sprite);
+            icon.value = sc.Icon;
+            icon.style.alignSelf = Align.Stretch;
 
-              Save.clicked += SaveItem;
-              Delete.clicked += DeleteItem;
-              leftList.RefreshItems();
-          }
-      }
-  }
+            
+
+            var dimensions = new Vector2IntField("Dimensions for item");
+            dimensions.RegisterValueChangedCallback(evt =>
+            {
+                Undo.RecordObject(sc, "Change Dimensions");
+                if (dimensions.value.x < 1)
+                {
+                    dimensions.value = new Vector2Int(1, dimensions.value.y);
+                }
+                else if (dimensions.value.y < 1)
+                {
+                    dimensions.value = new Vector2Int(dimensions.value.x, 1);
+                }
+                else if (dimensions.value.x > 9)
+                {
+                    dimensions.value = new Vector2Int(9, dimensions.value.y);
+                }
+                else if (dimensions.value.y > 6)
+                {
+                    dimensions.value = new Vector2Int(dimensions.value.x, 6);
+                }
+                else
+                {
+                    sc.SlotDimension.x = dimensions.value.x;
+                    sc.SlotDimension.y = dimensions.value.y;
+                    iconPre.style.height = sc.SlotDimension.y * 50;
+                    iconPre.style.width = sc.SlotDimension.x * 50;
+                    EditorUtility.SetDirty(sc);
+                }
+            });
+            dimensions.value = new Vector2Int(sc.SlotDimension.x, sc.SlotDimension.y);
+            dimensions.style.alignSelf = Align.Stretch;
+
+            var Save = new Button();
+            Save.text = "Save";
+            Save.style.marginTop = 30;
+            Save.style.alignSelf = Align.Center;
+            Save.style.maxWidth = 400;
+            Save.style.minWidth = 300;
+            Save.style.maxHeight = 20;
+            Save.style.minHeight = 20;
+            Save.style.flexGrow = 1;
+
+            var Delete = new Button();
+            Delete.text = "Delete Item";
+            Delete.style.marginTop = 10;
+            Delete.style.maxWidth = 400;
+            Delete.style.minWidth = 300;
+            Delete.style.alignSelf = Align.Center;
+            Delete.style.maxHeight = 20;
+            Delete.style.minHeight = 20;
+            Delete.style.flexGrow = 1;
+
+            m_RightPane.Add(Aname);
+            m_RightPane.Add(name);
+            m_RightPane.Add(rarity);
+            m_RightPane.Add(description);
+            m_RightPane.Add(sellprice);
+            m_RightPane.Add(dimensions);
+            m_RightPane.Add(icon);
+            //m_RightPane.Add(Save);
+            m_RightPane.Add(iconPre);
+            m_RightPane.Add(Delete);
+
+            Save.clicked += SaveItem;
+            Delete.clicked += DeleteItem;
+            leftList.RefreshItems();
+        }
+    }
+}
+   private void OnUndoRedoPerformed()
+   {
+       leftList.RefreshItems();
+
+       var Aname = m_RightPane[0] as TextField;
+       Aname.value = currentSc.name;
+
+       var name = m_RightPane[1] as TextField;
+       name.value = currentSc.FriendlyName;
+
+       var rarity = m_RightPane[2] as EnumField;
+       rarity.value = currentSc.Rarity;
+
+       var desc = m_RightPane[3] as TextField;
+       desc.value = currentSc.Description;
+
+       var price = m_RightPane[4] as IntegerField;
+       price.value = currentSc.SellPrice;
+
+       var dim = m_RightPane[5] as Vector2IntField;
+       dim.value = currentSc.SlotDimension;
+
+       var icon = m_RightPane[6] as ObjectField;
+       icon.objectType = typeof(Sprite);
+       icon.value = currentSc.Icon;
+
+       /*var iconPre = m_RightPane[6] as UnityEngine.UIElements.Image;
+       iconPre.sprite = icon.value as Sprite;
+       iconPre.style.height = currentSc.SlotDimension.y * 50;
+       iconPre.style.width = currentSc.SlotDimension.x * 50;*/
+   }
 
     //TODO: Fix this button
     private void SaveItem()
